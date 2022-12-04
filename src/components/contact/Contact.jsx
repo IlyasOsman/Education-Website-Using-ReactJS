938f0d7fc8330931c9a413af84b47ab35abb44e4
ilyasosman <earlyosman@gmail.com>
2022-12-04 15:39:55 +0300

    ...

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import Back from "../common/back/Back"
+import "./contact.css"
+
+const Contact = () => {
+  const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7906185174743!2d36.7824180151473!3d-1.3004808360095934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1a6bf7445dc1%3A0x940b62a3c8efde4c!2sMoringa%20School!5e0!3m2!1sen!2ske!4v1670156081484!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  return (
+    <>
+      <Back title='Contact us' />
+      <section className='contacts padding'>
+        <div className='container shadow flexSB'>
+          <div className='left row'>
+            <iframe src={map}></iframe>
+          </div>
+          <div className='right row'>
+            <h1>Contact us</h1>
+            <p>We're open for any suggestion or just to have a chat</p>
+
+            <div className='items grid2'>
+              <div className='box'>
+                <h4>ADDRESS:</h4>
+                <p>198 West 21th Street, Ngong Ln, Nairobi</p>
+              </div>
+              <div className='box'>
+                <h4>EMAIL:</h4>
+                <p> info@yoursite.com</p>
+              </div>
+              <div className='box'>
+                <h4>PHONE:</h4>
+                <p> + 254734 2355 98</p>
+              </div>
+            </div>
+
+            <form action=''>
+              <div className='flexSB'>
+                <input type='text' placeholder='Name' />
+                <input type='email' placeholder='Email' />
+              </div>
+              <input type='text' placeholder='Subject' />
+              <textarea cols='30' rows='10'>
+                Create a message here...
+              </textarea>
+              <button className='primary-btn'>SEND MESSAGE</button>
+            </form>
+
+            <h3>Follow us here</h3>
+            <span>FACEBOOK TWITTER INSTAGRAM DRIBBBLE</span>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Contact
